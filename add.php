74d0005d915ccd1d394271a1e0d7b439d0a60631
firdauzfan <firdauzfanani@gmail.com>
2019-01-17 11:29:20 +0700

    fix bugs

--- add.php
+++ add.php
@@ -4,7 +4,7 @@
 
 require_once 'app/init.php';
 
-if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date']) && isset($_POST['kendala']) && isset($_POST['progress'])) {
+if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date']) && isset($_POST['kendala'])) {
 	$name = trim($_POST['name']);
 	$name = escape($name);
 	$due_date = $_POST['due_date'];
@@ -13,7 +13,7 @@ if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date'])
 	$detail = $_POST['detail'];
 
 
-	if(!empty($name) && !empty($detail) && !empty($due_date) && !empty($kendala) && !empty($progress)) {
+	if(!empty($name) && !empty($detail) && !empty($due_date) && !empty($kendala)) {
 		$addedQuery = $db->prepare("
 			INSERT INTO items (name, detail, user, kendala, due_date, progress, done, delete_status, user_name, created)
 			VALUES (:name, :detail, :user, :kendala, :due_date, :progress, 0, 0, :user_name, NOW())
--- editsubmit.php
+++ editsubmit.php
@@ -4,7 +4,7 @@
 
 require_once 'app/init.php';
 
-if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date']) && isset($_POST['kendala']) && isset($_POST['progress'])) {
+if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date']) && isset($_POST['kendala'])) {
 	$name = trim($_POST['name']);
 	$name = escape($name);
 	$due_date = $_POST['due_date'];
@@ -13,7 +13,7 @@ if(isset($_POST['name']) && isset($_POST['detail']) && isset($_POST['due_date'])
   $itemedit = $_POST['iditem'];
   $detail = $_POST['detail'];
 
-  if(!empty($name) && !empty($due_date) && !empty($kendala) && !empty($progress)) {
+  if(!empty($name) && !empty($due_date) && !empty($kendala)) {
     $editQuery = $db->prepare("
       UPDATE `items` SET `name`=:name,`detail`=:detail,`kendala`=:kendala,`due_date`= :due_date,`progress`=:progress WHERE id=:id
     ");
